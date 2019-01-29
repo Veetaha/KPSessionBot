c792343d51545e98b754cf7a9217420d4b8d34bb
Veetaha <gerzoh1@gmail.com>
2019-01-29 19:59:28 +0200

    intermediate save

--- backend/models/academic-subject.ts
+++ backend/models/academic-subject.ts
@@ -1,32 +1,17 @@
 import * as Mongoose  from 'mongoose';
 import Paginate       from 'mongoose-paginate';
+import { CrudPlugin } from '@modules/mongoose-plugins/apollo-crud';
 import * as _         from 'lodash';
 import * as Utils     from '@modules/utils';
 
-import { CrudPlugin, CrudPluginStatics } from '@modules/mongoose-plugins/apollo-crud';
-import { MarkRange, Schema as MarkRangeSchema } from '@models/mark-range';
+
+import { Schema as AcademicActivitySchema } from '@models/academic-activity';
 import { 
-    MessageTemplates, 
-    Schema as MessageTemplatesSchema
-} from '@models/message-templates';
-import { Student } from './student';
-
-export interface MarkRanges {
-    practice: MarkRange;
-    credit:   MarkRange;
-    exam:     MarkRange;
-}
-
-export interface AcademicSubjectData {
-    name:     string;      // unique 
-    teachers: string[];    // teacher's names
-    // indicates the range of marks a student may get for respective activities
-    mark_ranges: MarkRanges;
-    mark_templates: {
-        positive: MessageTemplates;
-        negative: MessageTemplates;
-    };    
-}
+    AcademicSubjectStatics,
+    AcademicSubjectMethods,
+    AcademicSubjectModel,
+    AcademicSubjectDoc
+} from '@models/declarations/academic-subject';
 
 export const Schema = new Mongoose.Schema({
     name: { 
@@ -35,22 +20,10 @@ export const Schema = new Mongoose.Schema({
         index:    true, 
         unique:   true,
     },
-    teachers: { type: [String], required: true },
-    mark_ranges: {
-        type: {
-            practice: MarkRangeSchema,
-            credit:   MarkRangeSchema,
-            exam:     MarkRangeSchema,
-        },
-        required: true
-    },
-    mark_templates: {
-        type: {
-            positive: MessageTemplatesSchema,
-            negative: MessageTemplatesSchema,  
-        },
-        required: true
-    },
+    teachers: { type: [String],               required: true },
+    practice: { type: AcademicActivitySchema, required: true },
+    credit:   { type: AcademicActivitySchema, required: true },
+    exam:     { type: AcademicActivitySchema, required: true },
 });
 
 const Statics: AcademicSubjectStatics = {
@@ -61,7 +34,7 @@ const Statics: AcademicSubjectStatics = {
 
 const Methods: AcademicSubjectMethods = {
     randomMarkAndMessage(activityType, student) {
-        const mark = this.mark_ranges[activityType].random();
+        const mark = this
         return {
             mark, 
             message: this.mark_templates[mark > 0 ? 'positive' : 'negative']
@@ -85,35 +58,6 @@ Schema.methods = Methods;
 Schema.plugin(CrudPlugin);
 Schema.plugin(Paginate);
 
-export const AcademicSubject = Mongoose.model<AcademicSubject, AcademicSubjectModel>(
+export const AcademicSubject = Mongoose.model<AcademicSubjectDoc, AcademicSubjectModel>(
     'AcademicSubject', Schema
 );
-
-export interface MarkAndMessage {
-    mark:    number;
-    message: string;
-}
-
-export interface AcademicSubjectMethods {
-    randomMarkAndMessage(
-        this:         AcademicSubject,
-        activityType: keyof MarkRanges,
-        student:      Student
-    ): MarkAndMessage;
-}
-
-export interface AcademicSubject extends 
-Mongoose.Document, AcademicSubjectData, AcademicSubjectMethods {}
-
-export interface AcademicSubjectStatics {    
-    tryFindByName(
-        this: AcademicSubjectModel,
-        name: string
-    ): Promise<AcademicSubject>;
-}
-
-export interface AcademicSubjectModel extends 
-Mongoose.PaginateModel<AcademicSubject>, 
-AcademicSubjectStatics, 
-CrudPluginStatics<AcademicSubject> 
-{}
