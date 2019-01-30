5b1a4434de39ade2138912a8d6ae5ce31b83c8ed
Veetaha <veetaha2@gmail.com>
2019-01-30 20:38:31 +0200

    Added AcademicSubject db model (#22)
    * check branching feature
    
    * created MessageTemplates module
    
    * fixed a bug with escapeStringRegexp as it was hot
    
    * added global match options to MessageTemplates.render() regex
    
    * added convenient Utils module and Utils.pickRandom(arr) function
    
    * denied PreferReadonly rule refactor
    
    * Moved type declarations to /models/declarations/*.d.ts files, implemented AcademicActivity model
    
    * updated AcademicActivity schema definition
    
    * minor formatting changes
    
    * updated vee-type-safe dependency version
    
    * Added AcademicSubject db model. (#21)
    
    * added AcademicSubject model
    
    * intermediate save
    
    * save #2
    
    * implemented AcademicSubject
    
    * removed lodash unused import

---
+++ backend/models/academic-subject.ts
@@ -0,0 +1,62 @@
+import * as Mongoose  from 'mongoose';
+import Paginate       from 'mongoose-paginate';
+import * as Utils     from '@modules/utils';
+import { CrudPlugin } from '@modules/mongoose-plugins/apollo-crud';
+import { assert     } from '@modules/debug';
+
+import { Schema as AcademicActivitySchema } from '@models/academic-activity';
+import { 
+    AcademicSubjectStatics,
+    AcademicSubjectMethods,
+    AcademicSubjectModel,
+    AcademicSubjectDoc
+} from        '@models/declarations/academic-subject';
+export * from '@models/declarations/academic-subject';
+
+export const Schema = new Mongoose.Schema({
+    name: { 
+        type:     String, 
+        required: true, 
+        index:    true, 
+        unique:   true,
+    },
+    teachers: { type: [String],               required: true },
+    practice: { type: AcademicActivitySchema, required: true },
+    credit:   { type: AcademicActivitySchema, required: true },
+    exam:     { type: AcademicActivitySchema, required: true },
+});
+
+const Statics: AcademicSubjectStatics = {
+    tryFindByName(name) {
+        return this.tryFindOne({ name });
+    }
+};
+
+const Methods: AcademicSubjectMethods = {
+    randomMarkAndMessage(activityType, student) {
+        assert(['practice', 'exam', 'credit'].includes(activityType));
+
+        const activity = this[activityType];
+        const mark = activity.mark_range.random();
+        return {
+            mark, 
+            message: activity.renderRandomTemplateMessage({
+                mark,
+                student,
+                subject: this.name,
+                teacher: Utils.pickRandom(this.teachers)
+            })
+        };
+    }
+};
+
+Schema.statics = Statics;
+Schema.methods = Methods;
+
+// beware that plugins must come after assigning methods and statics to Schema
+Schema.plugin(CrudPlugin);
+Schema.plugin(Paginate);
+
+export const AcademicSubject = Mongoose.model<AcademicSubjectDoc, AcademicSubjectModel>(
+    'AcademicSubject', Schema
+);
